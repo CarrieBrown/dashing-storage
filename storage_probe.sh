5cf7bc32506c33586dcaf23d777d1953c8a365e3
cbohn <cbohn4@unl.edu>
2019-06-21 11:47:28 -0500

    Revert to original shell script after error

--- storage_probe.sh
+++ storage_probe.sh
@@ -3,7 +3,8 @@ source /util/opt/lmod/lmod/init/profile
 module use /util/opt/hcc-modules/Common
 module load anaconda
 source activate dashing
-#cd /util/admin/dashing-storage
+cd /util/admin/dashing-storage
 python /util/admin/dashing-storage/storage_probe.py
 conda deactivate
 module unload anaconda
+
